4696fbe954cb09f16a8d75ccdf0c168fa292e12f
SufiHarris <harrissofi02@gmail.com>
2024-09-04 10:41:34 +0530

    created user home page product page and service page and added bottom navigation bar

--- android/app/src/main/java/com/aseef/house_cleaning/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.aseef.house_cleaning;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- android/app/src/main/kotlin/com/aseef/house_cleaning/MainActivity.kt
+++ android/app/src/main/kotlin/com/aseef/house_cleaning/MainActivity.kt
@@ -2,4 +2,6 @@ package com.aseef.house_cleaning
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    // You can add any necessary overrides or custom code here
+}
