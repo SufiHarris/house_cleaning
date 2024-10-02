522127d8761d3bf34cdb190baeba6191af9c0b54
SufiHarris <harrissofi02@gmail.com>
2024-10-02 10:24:11 +0530

    added auth logic and some pages

--- lib/auth/provider/auth_provider.dart
+++ lib/auth/provider/auth_provider.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:shared_preferences/shared_preferences.dart'; // <-- Add this
-import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../../user/screens/user_main.dart';
+import '../../user/screens/user_create_profile_screen.dart';
 import '../screens/auth_screen.dart';
 
 class AuthProvider extends GetxController {
@@ -15,11 +17,19 @@ class AuthProvider extends GetxController {
   TextEditingController passwordController = TextEditingController();
 
   Rx<User?> user = Rx<User?>(null);
+  RxBool isLoading = false.obs; // Loader variable
 
   @override
   void onInit() {
     super.onInit();
     user.bindStream(_auth.authStateChanges());
+
+    ever(user, (User? currentUser) {
+      if (currentUser != null) {
+        // When user state changes, check if logged-in user is available
+        Get.offAll(() => const UserMain());
+      }
+    });
   }
 
   Future<bool> userExists(String email) async {
@@ -42,8 +52,11 @@ class AuthProvider extends GetxController {
       return;
     }
 
+    isLoading.value = true; // Show loader
+
     bool exists = await userExists(emailController.text.trim());
     if (!exists) {
+      isLoading.value = false; // Hide loader
       _showSnackBar('User does not exist. Please sign up.', true);
       return;
     }
@@ -66,94 +79,103 @@ class AuthProvider extends GetxController {
               userDoc.data() as Map<String, dynamic>;
           await _saveUserData(userData);
 
+          isLoading.value = false; // Hide loader
           Get.offAll(() => const UserMain());
           _showSnackBar('Logged in successfully', false);
         }
       }
     } on FirebaseAuthException catch (e) {
+      isLoading.value = false; // Hide loader
       String message = _getFirebaseErrorMessage(e.code);
       _showSnackBar(message, true);
     }
   }
 
-  Future<void> signInWithGoogle() async {
+  Future<void> initiateGoogleSignIn() async {
     try {
+      isLoading.value = true; // Show loader
+
+      // Trigger Google Sign-In, but don't authenticate with Firebase yet
       final GoogleSignInAccount? googleSignInAccount =
           await GoogleSignIn().signIn();
-      final GoogleSignInAuthentication? googleSignInAuthentication =
-          await googleSignInAccount?.authentication;
+      if (googleSignInAccount == null) {
+        // Handle case when Google sign-in is canceled
+        isLoading.value = false; // Hide loader
+        _showSnackBar('Google Sign-In was canceled.', true);
+        return;
+      }
 
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        idToken: googleSignInAuthentication?.idToken,
-        accessToken: googleSignInAuthentication?.accessToken,
-      );
+      final String googleEmail = googleSignInAccount.email;
 
-      UserCredential result = await _auth.signInWithCredential(credential);
-      User? userDetails = result.user;
+      // Check if the email exists in the Firestore user table
+      bool exists = await userExists(googleEmail);
 
-      if (userDetails != null) {
-        bool exists = await userExists(userDetails.email!);
-
-        if (exists) {
-          // User exists, fetch data and proceed to home screen
-          DocumentSnapshot userDoc = await _firestore
-              .collection('users_table')
-              .doc(userDetails.uid)
-              .get();
-
-          if (userDoc.exists) {
-            Map<String, dynamic> userData =
-                userDoc.data() as Map<String, dynamic>;
-            await _saveUserData(userData);
-
-            Get.offAll(() => const UserMain());
-            _showSnackBar('Logged in successfully', false);
-          }
-        } else {
-          // User does not exist, navigate to additional info screen
-          // Get.to(() => AdditionalInfoScreen(
-          //       onSubmit: (String phone, String address) async {
-          //         Map<String, dynamic> userInfoMap = {
-          //           "email": userDetails.email,
-          //           "name": userDetails.displayName,
-          //           "imgUrl": userDetails.photoURL,
-          //           "id": userDetails.uid,
-          //           "phone": phone,
-          //           "address": address,
-          //         };
-
-          //         // Save to Firestore
-          //         await _firestore
-          //             .collection('users_table')
-          //             .doc(userDetails.uid)
-          //             .set(userInfoMap);
-
-          //         // Save locally
-          //         await _saveUserData(userInfoMap);
-
-          //         Get.offAll(() => const UserMain());
-          //         _showSnackBar('New user registered successfully', false);
-          //       },
-          //     ));
-        }
+      if (exists) {
+        // If user exists, now perform the Google authentication
+        await _signInWithGoogleAuth(googleSignInAccount);
+      } else {
+        // If user does not exist, redirect to the create profile page
+        isLoading.value = false; // Hide loader
+        print(googleSignInAccount);
+        Get.to(() => CreateProfilePage(
+            // googleSignInAccount:
+            //     googleSignInAccount, // Pass Google Sign-In data
+            ));
       }
     } catch (e) {
+      isLoading.value = false; // Hide loader
       _showSnackBar('Google Sign-In failed. Please try again.', true);
     }
   }
 
-  // Save user data in SharedPreferences
-  Future<void> _saveUserData(Map<String, dynamic> userData) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('user_email', userData['email']);
-    await prefs.setString('user_name', userData['name']);
-    await prefs.setString('user_imgUrl', userData['imgUrl']);
-    await prefs.setString('user_id', userData['id']);
-    await prefs.setString('phone', userData['phone']);
-    await prefs.setString('address', userData['address']);
+// Perform the actual Google authentication after profile creation
+  Future<void> _signInWithGoogleAuth(
+      GoogleSignInAccount googleSignInAccount) async {
+    try {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken,
+      );
+
+      // Authenticate with Firebase using Google credentials
+      UserCredential result =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      User? userDetails = result.user;
+
+      if (userDetails != null) {
+        // Fetch user data from Firestore
+        DocumentSnapshot userDoc = await _firestore
+            .collection('users_table')
+            .doc(userDetails.uid)
+            .get();
+
+        if (userDoc.exists) {
+          Map<String, dynamic> userData =
+              userDoc.data() as Map<String, dynamic>;
+          await _saveUserData(userData);
+          Get.offAll(() => const UserMain());
+          _showSnackBar('Logged in successfully', false);
+        }
+      }
+    } catch (e) {
+      isLoading.value = false;
+      _showSnackBar('Authentication failed. Please try again.', true);
+    }
   }
 
-  // Helper function to show snack bars
+  Future<void> _saveUserData(Map<String, dynamic> userData) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('user_email', userData['email'] ?? '');
+    await prefs.setString('user_name', userData['name'] ?? '');
+    await prefs.setString('user_imgUrl', userData['imgUrl'] ?? '');
+    await prefs.setString('user_id', userData['id'] ?? '');
+    await prefs.setString('phone', userData['phone'] ?? '');
+    await prefs.setString('address', userData['address'] ?? '');
+  }
+
   void _showSnackBar(String message, bool isError) {
     ScaffoldMessenger.of(Get.context!).showSnackBar(
       SnackBar(
@@ -163,50 +185,20 @@ class AuthProvider extends GetxController {
     );
   }
 
-  // Function to sign out and clear local data
   Future<void> signOut() async {
     try {
-      await _auth.signOut(); // Firebase sign out
-      await GoogleSignIn().signOut(); // Google sign out
+      await _auth.signOut();
+      await GoogleSignIn().signOut();
 
-      // Clear SharedPreferences
       SharedPreferences prefs = await SharedPreferences.getInstance();
-      await prefs.clear(); // Clear all saved data
+      await prefs.clear();
 
-      // Clear the user instance and navigate to AuthScreen
       user.value = null;
       Get.offAll(() => const AuthScreen());
 
       _showSnackBar('Logged out successfully', false);
     } catch (e) {
       _showSnackBar('Failed to log out. Please try again.', true);
-    }
-  }
-
-  Future<void> resetPassword(String email) async {
-    if (email.isEmpty) {
-      _showSnackBar('Please enter your email address', true);
-      return;
-    }
-
-    try {
-      await _auth.sendPasswordResetEmail(email: email);
-      _showSnackBar(
-          'Password reset email sent. Please check your inbox.', false);
-    } on FirebaseAuthException catch (e) {
-      String message;
-      switch (e.code) {
-        case 'invalid-email':
-          message = 'The email address is badly formatted.';
-          break;
-        case 'user-not-found':
-          message = 'No user found for that email.';
-          break;
-        default:
-          message =
-              'An error occurred while resetting password. Please try again.';
-      }
-      _showSnackBar(message, true);
     }
   }
 
@@ -219,7 +211,7 @@ class AuthProvider extends GetxController {
       case 'invalid-email':
         return 'The email address is badly formatted.';
       default:
-        return 'An undefined error happened. Please try again.';
+        return 'An error occurred. Please try again.';
     }
   }
 }
